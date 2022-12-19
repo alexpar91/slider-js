c55a8367f28b46ff57ed851064530ee6fba1727c
alexpar91 <74785349+alexpar91@users.noreply.github.com>
2022-12-20 02:39:05 +0300

    start

--- js/script.js
+++ js/script.js
@@ -11,48 +11,41 @@ let current = 0;
 const btnNext = document.querySelector('.next');
 
 btnNext.onclick = function next() {
-	for (let i = 0; i < sliderItem.length; i++) {
-		sliderItem[i].classList.remove('active');
-		dotsItem[i].classList.remove('active');
-	}
+    for (let i = 0; i < sliderItem.length; i++){
+        sliderItem[i].classList.remove('active');
+        dotsItem[i].classList.remove('active');
+    }
 
-
-	if (current >= sliderItem.length - 1) {
-		current = 0;
-		btnControl.disabled = false;
-	}
-	else {
-		current++;
-	}
-
-	sliderItem[current].classList.add('active');
-	dotsItem[current].classList.add('active');
-
+    if (current >= sliderItem.length-1) {
+        current = 0;
+        btnControl.disabled = false;
+    }
+    else {
+        current++;
+    } 
+    sliderItem[current].classList.add('active');
+    dotsItem[current].classList.add('active');  
 }
 
 const btnPrev = document.querySelector('.prev');
 
 btnPrev.onclick = function prev() {
 
-	if (current <= 0) {
-		current = sliderItem.length - 1;
+    if (current <= 0){
+        current = sliderItem.length-1;      
+    }
+    else {
+        current--;
+    }
 
-	}
-	else {
-		current--;
-	}
+    for (let i = 0; i < sliderItem.length; i++){
+        sliderItem[i].classList.remove('active');
+        dotsItem[i].classList.remove('active');
+        }
+    sliderItem[current].classList.add('active');
+    dotsItem[current].classList.add('active');
 
-	for (let i = 0; i < sliderItem.length; i++) {
-		sliderItem[i].classList.remove('active');
-		dotsItem[i].classList.remove('active');
-	}
-	sliderItem[current].classList.add('active');
-	dotsItem[current].classList.add('active');
-
-	if (current == 0) {
-		btnControl.disabled = false;
-	}
-
+    if (current == 0){
+        btnControl.disabled = false;
+    }    
 }
-
-
